d1f19999ac3122eb233cad51e641ab181b1175c8
Zubair <57180013+MZubairLohar@users.noreply.github.com>
2025-02-20 20:28:29 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
       <Solidarity />
       <Mentality />
       <Decentralization />
-      {/* <CombinedLevel /> */}
       <IndividualLevel />
       {/* <Registration /> */}
     </main>
